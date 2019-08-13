2bdeb4b59da6e013bf3af642394e411f47372bf6
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-13 14:00:53 -0700

    Added imported data to state and used map() to iterate over the numbers array. Steps two and three completed

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 //import any components needed
 
-import numberButton from  './NumberButton'
+import NumberButton from  './NumberButton'
 //Import your array data to from the provided data file
 
 import {numbers} from './../../../data'
@@ -10,11 +10,18 @@ import {numbers} from './../../../data'
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numbersState, setNumbersState] = useState(numbers);
+  //console.log ('numbers', { numbersState });
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {numbersState.map((number, index) => (
+        <NumberButton numbers = {number} key = {index} />
+      ))}
+
     </div>
   );
 };
+
+
